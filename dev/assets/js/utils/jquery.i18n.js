73e2e69fe6e682c4746fdb5e4789e1a0927b0d97
Jorge Silva <jorge.do.carmo123@gmail.com>
2021-05-04 15:14:43 -0300

    Language preferences

--- dev/assets/js/utils/jquery.i18n.js
+++ dev/assets/js/utils/jquery.i18n.js
@@ -1,12 +1,23 @@
 $(function() {
-    $.i18n( {
-        locale: 'en'
-    });
+    var userLang = navigator.language || navigator.userLanguage;
+    var langSelectedKey = 'langSelected';
+    var locale = 'en';
+    var langSelected = localStorage.getItem(langSelectedKey);
 
+    if (langSelected) {
+        locale = langSelected;
+     } else if (userLang === 'pt-BR') {
+        locale = 'pt-BR';
+    }
+    
+    $.i18n( {
+        locale: locale
+    });
     $('body').i18n();
 
     changeLocale = function(localeArg) {
         $.i18n().locale = localeArg;
         $('body').i18n();
+        localStorage.setItem(langSelectedKey, localeArg);
     };
 });
