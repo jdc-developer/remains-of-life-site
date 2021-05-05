bf61f117d087a9345a3f108748bc0043a08593d8
Jorge Silva <jorge.do.carmo123@gmail.com>
2021-05-05 14:16:35 -0300

    Adjustments responsivity tablets

--- dev/assets/js/pages/photos.js
+++ dev/assets/js/pages/photos.js
@@ -1,7 +1,7 @@
 $(function(){
     for (var i = 1; i < 19; i++) {
         $('.photos').append(
-        '<div class="col-md-4 col-xs-12 mt-2">' +
+        '<div class="col-md-4 col-xs-12 col-sm-6 mt-2">' +
             '<a href="assets/images/img-' + i + '.jpg" data-lightbox="remains-of-life-photos" data-title="Photo ' + i + '">' +
                 '<img class="img-album" src="assets/images/img-' + i + '.jpg" alt="Remains of Life Img ' + i + '">' +
             '</a>' +
