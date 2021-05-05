179908d77dd1b13f8b12e2a85befc1e75652b284
Jorge Silva <jorge.do.carmo123@gmail.com>
2021-05-05 18:35:26 -0300

    Not found page

--- dev/assets/js/i18n/pt-BR.js
+++ dev/assets/js/i18n/pt-BR.js
@@ -17,6 +17,8 @@ $(function() {
 		'biography-content-3': 'Pilgrimage é um álbum parcialmente conceitual. Este não necessariamente narra eventos de uma história delineada em arquétipos, ou de um personagem, mas sim, é uma grande junção de sentimentos progressivos, os quais o autor, de fato sentiu, e os descreveu na obra; através de uma persona imaginativa fazendo o papel do personagem conceitual, criando imagens abstratas para representar os obstáculos e as nuances destes sentimentos.',
 		'biography-content-4': 'A sonoridade do álbum mescla os mais variados arranjos do metal extremo. As guitarras, apresentam-se em marcantes linhas harmônicas, e em outras linhas agressivas, diretas e extremamente técnicas; o violão de nylon, uma paixão do autor, está presente brilhante e intenso em diversos pontos da obra; e as vozes variam do mais pungente gutural ao mais sublime e melódico harmonizado.',
 		'biography-content-5': 'O conceito do projeto é narrar sentimentos e situações sob óticas abstratas sem perder sua essência, e sem necessariamente terem sido sentidas pelo autor, mas também compreendidas e expandidas por ele. Tudo isso através da arte musical. É por isso que o projeto foi intitulado Remains of Life (Restos da Vida), pois o que resta de uma vida além de lembranças e sentimentos?',
-		'contact-content': 'Se deseja contatar a banda, por favor envie um email para o endereço'
+		'contact-content': 'Se deseja contatar a banda, por favor envie um email para o endereço',
+		'not-found': 'Não encontrado',
+		'not-found-content': 'A página que você estava procurando não existe.'
     }, 'pt-BR');
 });
