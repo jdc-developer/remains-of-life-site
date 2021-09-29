9c1b028bc98b7902c1ad7578cff0070378d9f088
Jorge do Carmo <jorge.do.carmo123@gmail.com>
2021-09-28 21:26:02 -0300

    First single edit

--- dev/assets/js/i18n/en.js
+++ dev/assets/js/i18n/en.js
@@ -5,7 +5,7 @@ $(function() {
 		'photos': 'Photos',
 		'videos': 'Videos',
 		'contact': 'Contact',
-		'band-description': 'A one-man band from Brazil',
+		'band-description': 'Progressive Death Metal from Brazil',
 		'pilgrimage-text': '(to be released soon, 2021)',
 		'development-credits': 'Website developed by Jorge Silva.',
 		'rights-message': 'All rights reserved.',
@@ -13,16 +13,15 @@ $(function() {
 		'news': 'News',
 		'channels': 'Channels',
 		'biography-content-1': 'Remains of Life is a one-man band from São Paulo – SP, Brazil founded by the musician and producer Jorge Silva. After small amateur projects between friends, amongst these, the so called Illuriah and Tribal Blood, Silva decided to create his own musical project in order to achieve his artistic ambitions.',
-		'biography-content-2': 'Mixing old and new compositions, Remains of Life was idealized towards the end of 2018 and the start of 2019, when Silva started the compositions and recordings in his home. After much work and many trials, Silva concluded the first work of the band, which is the full-length Pilgrimage.',
+		'biography-content-2': 'Mixing old and new compositions, Remains of Life was idealized towards the end of 2018 and the start of 2019, when Silva started the compositions and recordings in his home. After much work and many trials, Silva concluded the first album of the band, which is the full-length Pilgrimage.',
 		'biography-content-3': 'Pilgrimage is a partially conceptual album. It doesn’t necessarily relates the events of a story or of a character outlined in archetypes, but instead, it is a great mixture of progressive feelings, which all of those were factually felt by the author, and he describes them in the work; throughout an imaginative persona playing the role of a conceptual character, creating abstract images to represent the obstacles and the shapes of those feelings.',
 		'biography-content-4': 'The album sonority mingles the most variated extreme metal arrangements. The guitars, present themselves with remarkable harmonic lines, and other heavy, straight and extremely technical lines; the nylon guitar, one of the author passions, is shown up brilliant and intense in many moments of the work; and the voices vary from the most poignant growling to the most lofty melodic and harmonic.',
 		'biography-content-5': 'The project’s concept is to relate feelings and situations under metaphoric optics without losing their essence and without these being necessarily felt and lived by the author, but also comprehended and expanded by him; all of this through the musical art. For such a reason, the project has been entitled Remains of Life, for what remains of a life but memories and feelings?',
 		'contact-content': 'If you wish to contact the band, please send an email to the address',
 		'not-found': 'Not found',
 		'not-found-content': 'The page you were looking for does not exist.',
-		'new-single': 'New Single Out',
-		'new-single-content': 'New Single Out',
+		'new-single': 'First Single Out',
 		'new-single-content-1': 'Remains of Life announces its brand new single, Shattered Mirror. This song was the first composition of the project founder, Jorge Silva. Its original arrangements, created long before the band\'s conception, took short passages through the first bands which Silva featured in. Being his first work, this one received a special care from its author, being invariably chosen to feature as the unique single from Remains of Life\'s debut album.',
-		'new-single-content-2': 'Shattered Mirror is a slow and exceptionally melancolic song, even for the high dramatic standards of the album. You may check it on the main digital platforms and on the band\'s YouTube channel, where we uploaded an amazing lytic video produced by Wolff Videomaker. We wish to thank Alef Marra for the cover art, Marcel Briani for the edition and Victor Zavinsck for the logo drawing. You can watch the lyric video below:'
+		'new-single-content-2': 'Shattered Mirror is a slow and exceptionally melancolic song, even for the high dramatic standards of the album. Its pacing goes through melodic phrases and intense and explosive guitar arrangements. You may check it on the main digital platforms and on the band\'s YouTube channel, where we uploaded an amazing lyric video produced by Wolff Videomaker. We wish to thank Alef Marra for the cover art, Marcel Briani for the edition and Victor Zavinsck for the logo drawing. You can watch the lyric video below:'
     }, 'en');
 });
--- dev/assets/js/i18n/pt-BR.js
+++ dev/assets/js/i18n/pt-BR.js
@@ -5,7 +5,7 @@ $(function() {
 		'photos': 'Fotos',
 		'videos': 'Vídeos',
 		'contact': 'Contato',
-		'band-description': 'Uma banda-de-um-homem do Brasil',
+		'band-description': 'Progressive Death Metal do Brasil',
 		'pilgrimage-text': '(lançamento em breve, 2021)',
 		'development-credits': 'Site desenvolvido por Jorge Silva.',
 		'rights-message': 'Todos os direitos reservados.',
@@ -13,15 +13,15 @@ $(function() {
 		'news': 'Notícias',
 		'channels': 'Canais',
 		'biography-content-1': 'Remains of Life é uma banda-de-um-homem de São Paulo – SP, Brasil fundada pelo músico e produtor Jorge Silva em 2019. Após pequenos projetos amadores entre amigos, dentre estes, os chamados Illuriah e Tribal Blood, Silva resolveu criar seu próprio projeto musical a fim de realizar suas ambições artísticas.',
-		'biography-content-2': 'Mesclando composições antigas e novas, a Remains of Life foi idealizada por volta do final de 2018 e início de 2019, quando Silva iniciou as composições e gravações em sua casa. Após muito trabalho e repetidas investidas, Silva concluiu o primeiro trabalho da banda, que é o álbum Pilgrimage.',
+		'biography-content-2': 'Mesclando composições antigas e novas, a Remains of Life foi idealizada por volta do final de 2018 e início de 2019, quando Silva iniciou as composições e gravações em sua casa. Após muito trabalho e repetidas investidas, Silva concluiu a primeira obra da banda, que é o álbum Pilgrimage.',
 		'biography-content-3': 'Pilgrimage é um trabalho parcialmente conceitual. Este não necessariamente narra eventos de uma história ou de um personagem delineado em arquétipos, mas sim, é uma grande junção de sentimentos progressivos, os quais o autor de fato sentiu, e os descreveu na obra; através de uma persona imaginativa fazendo o papel do personagem conceitual, criando imagens abstratas para representar os obstáculos e as nuances desses sentimentos.',
 		'biography-content-4': 'A sonoridade do álbum mescla os mais variados arranjos do metal extremo. As guitarras, apresentam-se em marcantes linhas harmônicas, e em outras linhas agressivas, diretas e extremamente técnicas; o violão de nylon, uma paixão do autor, está presente brilhante e intenso em diversos pontos da obra; e as vozes variam do mais pungente gutural ao mais sublime e melódico harmonizado.',
 		'biography-content-5': 'O conceito do projeto é narrar sentimentos e situações sob óticas metafóricas sem perder sua essência e sem que estas tenham sido necessariamente sentidas e vividas pelo autor, mas também compreendidas e expandidas por ele; tudo isso através da arte musical. Por tal razão, o projeto foi intitulado Remains of Life (Restos da Vida), pois o que resta de uma vida além de lembranças e sentimentos?',
 		'contact-content': 'Se deseja contatar a banda, por favor envie um email para o endereço',
 		'not-found': 'Não encontrado',
 		'not-found-content': 'A página que você estava procurando não existe.',
-		'new-single': 'Lançamento do Novo Single',
+		'new-single': 'Lançamento do Primeiro Single',
 		'new-single-content-1': 'A Remains of Life anuncia o seu primeiro single, Shattered Mirror. Esta música foi a primeira composição do fundador do projeto, Jorge Silva. Seus arranjos originais, criados muito antes da concepção da banda, marcaram pequenas passagens pelas primeiras bandas das quais Silva participou. Sendo sua primeira obra, esta recebeu um carinho especial de seu autor, sendo invariavelmente escolhida para figurar como single único do álbum de estreia da Remains of Life.',
-		'new-single-content-2': 'Shattered Mirror é uma canção lenta e excepcionalmente melancólica, até mesmo para os padrões altamente dramáticos do álbum. Você pode conferi-la nas principais plataformas digitais e no canal do YouTube da banda, onde disponibilizamos um incrível lyric video produzido pelo Wolff Videomaker. Devemos nossos agradecimentos à Alef Marra pela arte da capa, Marcel Briani pela arte finalista e Victor Zavinsck pelo desenho do logo. Assista ao lyric video abaixo:'
+		'new-single-content-2': 'Shattered Mirror é uma canção lenta e excepcionalmente melancólica, até mesmo para os padrões altamente dramáticos do álbum. Seu andamento se dá através de frases melódicas e arranjos de guitarra intensos e explosivos. Você pode conferi-la nas principais plataformas digitais e no canal do YouTube da banda, onde disponibilizamos um incrível lyric video produzido pelo Wolff Videomaker. Devemos nossos agradecimentos à Alef Marra pela arte da capa, Marcel Briani pela arte finalista e Victor Zavinsck pelo desenho do logo. Assista ao lyric video abaixo:'
     }, 'pt-BR');
 });
