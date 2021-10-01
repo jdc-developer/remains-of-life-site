dfb168c1cb124697eec35a090fb112056e69860e
Jorge do Carmo <jorge.do.carmo123@gmail.com>
2021-10-01 14:25:25 -0300

    Changed text and photo gallery

--- dev/assets/js/pages/photos.js
+++ dev/assets/js/pages/photos.js
@@ -1,5 +1,5 @@
 $(function(){
-    for (var i = 1; i < 19; i++) {
+    for (var i = 1; i < 16; i++) {
         $('.photos').append(
         '<div class="col-md-4 col-xs-12 col-sm-6 mt-2">' +
             '<a href="assets/images/img-' + i + '.jpg" data-lightbox="remains-of-life-photos" data-title="Photo ' + i + '">' +
